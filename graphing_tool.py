a0977f7091a74e8dfd4813afc0e68f1d005e1e6d
DataDrivenProf <lsamalot@hotmail.com>
2025-04-12 22:41:37 -0400

    will save this onto the master branch after

--- graphing_tool.py
+++ graphing_tool.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import openpyxl
 
-excel_file = "bio107_lab_student_data.xlsx"
+excel_file = r"C:\Users\lsama\coding_c_drive\Graphing_Tool\bio107_lab_student_data.xlsx"
 df_1= pd.read_excel(excel_file)
 
 # get the column names of the df
