f04c062355a0fdd438918c984a634179a2ad5c70
DataDrivenProf <lsamalot@hotmail.com>
2025-04-25 19:56:39 -0400

    Removed the import streamlit as st line

--- graphing_tool.py
+++ graphing_tool.py
@@ -4,17 +4,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import openpyxl
 
-excel_file = "C:\\Users\\lsama\\OneDrive\\Documents\\Teaching\\HCC\\Laboratory\\Lab Manual\\Lab 1 Measurements\\bio107_lab_student_data.xlsx"
+excel_file = r"C:\Users\lsama\coding_c_drive\Graphing_Tool\bio107_lab_student_data.xlsx"
 df_1= pd.read_excel(excel_file)
+
 
 # get the column names of the df
 column_names = df_1.columns.to_list() # make list out of column names
 df_2 = pd.DataFrame({"index": range(len(column_names)), #create a range for the index column of the DataFrame_2
                   "columns": column_names})
 print(f"DataFrame 2\n", df_2) # display the dataframe so that the user can pick the x and y column of data
+
+
 # Pick your index from the df_2 for your x-axis
 x = int(input("which column will be the x-axis data, according to the df_2 index?")) #integer for picking x-axix df_1 column
 y_column_num = int(input("How many y-axis columns are you plotting with the same x-axis?"))
+
 r = list(range(1, y_column_num +1)) # make a list from which to iterate from
 
 for i in r:
@@ -23,6 +27,7 @@ for i in r:
     slope, intersect, r_value, p_value, std_err = linregress(df_1.iloc[:, x], df_1.iloc[:, y_column_selection]) 
     y_pred = df_1.iloc[:, x] * slope + intersect
 
+
     # graphing section
     plt.figure(i)
     plt.scatter(df_1.iloc[:, x], df_1.iloc[:, y_column_selection])
@@ -30,4 +35,3 @@ for i in r:
     plt.title(f"{df_1.columns[x]} vs {df_1.columns[y_column_selection]}")
     plt.xlabel(f"{df_1.columns[x]} (cm)")
     plt.ylabel(f"{df_1.columns[y_column_selection]} (cm)")
-    plt.show()
